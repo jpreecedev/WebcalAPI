75ed05a17fd8d054bd31f663686d18ea85498926
Jon Preece <jon@developerhandbook.com>
2016-02-29 10:48:23 +0000

    Added basic API authentication (not implemented username/password checking)

---
+++ WebcalAPI/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace WebcalAPI
+{
+    public class FilterConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Filters.Add(new AuthorizeAttribute());
+        }
+    }
+}
