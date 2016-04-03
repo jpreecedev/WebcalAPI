e5ad298ad942326b2df47e0e41a9f2e929ffecce
Jon Preece <jonpreece@hotmail.co.uk>
2016-04-03 12:24:21 +0100

    Added "Centre Check"

---
+++ WebcalAPI/Controllers/CentreCheckController.cs
@@ -0,0 +1,21 @@
+﻿namespace WebcalAPI.Controllers
+{
+    using System.Linq;
+    using System.Web.Http;
+    using Connect.Shared.Models;
+    using Core;
+    using Models;
+
+    [RoutePrefix("api/centrecheck")]
+    public class CentreCheckController : BaseApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            using (var context = new ConnectContext())
+            {
+                return Ok(context.GetReports<QCReport6Month>(ConnectUser).Select(c => new CentreCheckViewModel(c)));
+            }
+        }
+    }
+}
---
+++ WebcalAPI/Models/CentreCheckViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace WebcalAPI.Models
+{
+    using System;
+    using Connect.Shared;
+    using Connect.Shared.Models;
+
+    public class CentreCheckViewModel
+    {
+        public CentreCheckViewModel(QCReport6Month qcReport6Month)
+        {
+            if (qcReport6Month == null)
+            {
+                return;
+            }
+
+            CentreName = qcReport6Month.CentreName;
+            SealNumber = qcReport6Month.CentreSealNumber;
+            Date = qcReport6Month.Date;
+            Name = qcReport6Month.Name;
+            DocumentId = qcReport6Month.Id;
+            DocumentTypeEnum = DocumentType.QCReport6Month;
+        }
+
+        public string CentreName { get; set; }
+        public string SealNumber { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public int DocumentId { get; set; }
+        public DocumentType DocumentTypeEnum { get; set; }
+    }
+}
