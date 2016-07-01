cbe0771a0aa343142f6c93dcd8fff38b64c06514
Jon Preece <jonpreece@hotmail.co.uk>
2016-07-01 15:13:04 +0100

    Un-pork branch

---
+++ Webcal.Model/CalibrationEmailViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Webcal.Model
+{
+    using System.Collections.Generic;
+
+    public class CalibrationEmailViewModel
+    {
+        public string Recipient { get; set; }
+
+        public ICollection<RecentCalibrationsViewModel> Calibrations { get; set; }
+    }
+}
