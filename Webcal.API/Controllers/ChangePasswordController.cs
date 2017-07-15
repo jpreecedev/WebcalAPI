4ac8c6a4b3b22ca00fa8e4d941aa53d6a2d4e29f
Jon Preece <jonpreece@hotmail.co.uk>
2017-07-15 18:29:59 +0100

    Added ability to change password

---
+++ Webcal.API/Controllers/ChangePasswordController.cs
@@ -0,0 +1,26 @@
+﻿namespace Webcal.API.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using System.Web.Http;
+    using Core;
+    using Model;
+
+    [RoutePrefix("api/changepassword")]
+    public class ChangePasswordController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody] ChangePasswordViewModel data)
+        {
+            using (var context = new ConnectContext())
+            {
+                var result = await UserManager.ChangePasswordAsync(ConnectUser.Id, data.CurrentPassword, data.NewPassword);
+                if (result.Succeeded)
+                {
+                    return Ok();
+                }
+                return BadRequest(string.Join(",", result.Errors));
+            }
+        }
+    }
+}
---
+++ Webcal.Model/ChangePasswordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Webcal.Model
+{
+    public class ChangePasswordViewModel
+    {
+        public string CurrentPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
